e0b259eecfefd9b82f807ceef798ad249205f4e8
xhlulu <xhlperso@gmail.com>
2021-01-26 01:01:56 -0500

    Add image and logo

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import { useEffect, useState, useRef } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import github from './images/GitHub.png';
 
 import trainScript from './python/train.py';
 import predScript from './python/predict.py';
+
+const urls = {
+  virginica: 'https://upload.wikimedia.org/wikipedia/commons/9/9f/Iris_virginica.jpg',
+  versicolor: 'https://upload.wikimedia.org/wikipedia/commons/a/ab/Iris_versicolor_Quebec_2.jpg',
+  setosa: 'https://upload.wikimedia.org/wikipedia/commons/5/56/Kosaciec_szczecinkowaty_Iris_setosa.jpg'
+}
 
 const round = (n) => Number.parseFloat(n).toFixed(2);
 
@@ -43,6 +50,7 @@ function App() {
   const [petWid, setPetWid] = useState(1.25);
 
   const [label, setLabel] = useState("(loading...)");
+  const [imgSrc, setImgSrc] = useState();
 
   useEffect(() => {
     window.languagePluginLoader
@@ -70,19 +78,27 @@ function App() {
           };
           const predicted = window.pyodide.runPython(code);
           setLabel(predicted);
+          setImgSrc(urls[predicted]);
         });
       })
   }, [sepLen, sepWid, petLen, petWid]);
 
   return (
+    
     <div className="App">
+      <div className="App-github">
+        <a href="https://github.com/xhlulu/react-pyodide-iris"><img src={github} height="100%" width="100%"/></a>
+      </div>
       <div className="App-content">
-        <img src={logo} className="App-logo" alt="logo" />
+        <h2>Iris Classifier (with pyodide and scikit-learn)</h2>
         <div>
           <AnnotatedSlider name="Sepal Length" min={4} max={8} value={sepLen} setter={setSepLen} />
           <AnnotatedSlider name="Sepal Width" min={2} max={4.5} value={sepWid} setter={setSepWid} />
           <AnnotatedSlider name="Petal Length" min={1} max={7} value={petLen} setter={setPetLen} />
           <AnnotatedSlider name="Petal Width" min={0} max={2.5} value={petWid} setter={setPetWid} />
+        </div>
+        <div className="App-image">
+          <img src={imgSrc} height="100%"></img>
         </div>
         <div>Predicted label: {label}</div>
       </div>
